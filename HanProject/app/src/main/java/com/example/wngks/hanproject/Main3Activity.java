b3027cbb1b150cb13c2ae1735db1cab84733478d
JooHanEum <wngksdma123@gmail.com>
2018-08-06 18:30:39 +0900

    HanProject(수정)

--- HanProject/app/src/main/java/com/example/wngks/hanproject/Main3Activity.java
+++ HanProject/app/src/main/java/com/example/wngks/hanproject/Main3Activity.java
@@ -1,19 +1,66 @@
 package com.example.wngks.hanproject;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
 
 public class Main3Activity extends AppCompatActivity {
 
-
+    ImageView img1, img2, img3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        img1 = findViewById(R.id.img1);
+        img2 = findViewById(R.id.img2);
+        img3 = findViewById(R.id.img3);
+
+        img1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main3Activity.this, MainActivity.class);
+                intent.putExtra("img", "img_a");
+                startActivity(intent);
+            }
+
+        });
+
+        img2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main3Activity.this, MainActivity.class);
+                intent.putExtra("img", "img_b");
+                startActivity(intent);
+            }
+
+        });
+
+        img3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Main3Activity.this, MainActivity.class);
+                intent.putExtra("img", "img_c");
+                startActivity(intent);
+            }
+
+        });
+
+
     }
 
     @Override
--- HanProject/app/src/main/java/com/example/wngks/hanproject/MainActivity.java
+++ HanProject/app/src/main/java/com/example/wngks/hanproject/MainActivity.java
@@ -1,13 +1,32 @@
 package com.example.wngks.hanproject;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.nio.channels.InterruptedByTimeoutException;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView img;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        img = findViewById(R.id.img_main);
+        Intent i = getIntent();
+        String str = i.getStringExtra("img");
+        int resID = res.getIdentifier(str, "drawable", getPackageName());
+        Drawable drawable = getDrawable(resID);
+        img.setBackground(drawable);
     }
 }
